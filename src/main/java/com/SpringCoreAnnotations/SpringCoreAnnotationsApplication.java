a2624612833ca2406280e41b7f29fdc34fd1ac19
bandar saud <bandar.programer@gmail.com>
2023-03-11 05:19:37 +0300

    add text

--- src/main/java/com/SpringCoreAnnotations/SpringCoreAnnotationsApplication.java
+++ src/main/java/com/SpringCoreAnnotations/SpringCoreAnnotationsApplication.java
@@ -12,6 +12,41 @@ import com.SpringCoreAnnotations.lazy.LazyLoader;
 
 @SpringBootApplication
 public class SpringCoreAnnotationsApplication {
+	
+/*
+	 
+	 Spring offer 3 type of configuration
+- XML based configuration
+- Annotation based configuration
+- java based configuration 
+
+@Component annotation tells spring container to automatically create spring bean
+@Autowired annotation is used to inject the bean automatically (used {constructor-setterfield} injection)
+@Qualifier annotation is used in conjunction with autowired to avoid confusion when we have two or more beans configured for same taype
+@Primary Annotation to give higher preference to a bean when there are multiple beans of the same type (same as Qualifier but on class level)
+
+Stereotype annotations
+- These annotations are used to create spring beans automatically in the application context(Spring IoC container)
+- The main stereotype annotation is @Component
+- By using this annotation, spring provides more stereotype meta annotation such as @Service @Repository @Controller
+
+@Lazy annotation by default, spring creates all singleton beans eagerly at the startup/bootstrapping of the application context.
+you can load the spring beans lazily (on-demand) using @lazy annotation
+@Lazy annotation can used with @Configuration, @Component and @Bean annotation
+- Eager intialization is recommended to avoid and detect all possible errors immediately rather than at runtime
+
+
+----- Spring Bean Scopes -----
+@Scope annotation is used to define a scope of the bean. We use @Scope to define the scope of a @Component class or @Bean definition
+- singleton : only one instance of the bean is created and shared across the entire application. this is the default scope
+prototype : a new instance of the bean is created every time is it  requested
+request : 
+seasion
+application
+websocket
+
+
+	 */
 
 	public static void main(String[] args) {
 		var context = SpringApplication.run(SpringCoreAnnotationsApplication.class, args);
